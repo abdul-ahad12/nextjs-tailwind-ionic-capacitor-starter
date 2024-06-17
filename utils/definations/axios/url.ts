d46a021b7e4fe62898e4e6a02f26a82c70007927
abdul-ahad12 <abdulkouzer@gmail.com>
2024-06-17 13:47:09 +0530

    URL

--- utils/definations/axios/url.ts
+++ utils/definations/axios/url.ts
@@ -2,4 +2,4 @@ const url = 'https://api.inspectly.com.au';
 const version = '/api/v1';
 export const baseURL = `${url}${version}`;
 export const socketURL = `https://chat.inspectly.com.au`;
-export const phoneCode="+91"
+export const phoneCode="+61"
